f75357c2dd665e491fc3c4d8db482e28568a5b90
Alessandro <alebaldissera99@gmail.com>
2023-05-16 00:18:14 +0200

    Implementata classe App - close #8

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -2,15 +2,23 @@
 // Silvia Rovea 1223863
 // Alessandro Baldissera 1216742
 ////////////////////////////////////////////////////////////////////
-
 package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        try {
+            System.out.print("Inserisci un numero da convertire: ");
+            int num = scan.nextInt();
+            System.out.println(RomanPrinter.print(num));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scan.close();
+        }
     }
 }
